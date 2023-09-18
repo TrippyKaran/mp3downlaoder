a1287fdc4685961652d1210d8eda2fa398637180
unknown <karan.trippybug@gmail.com>
2023-09-18 16:25:46 +0530

    readme deleted

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify,send_file
+from flask_cors import CORS 
+from pytube import YouTube
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/download_audio', methods=['POST'])
+def download_audio():
+    try:
+        data = request.get_json()
+        link = data.get('link')
+        youtube_object = YouTube(link)
+        audio_stream = youtube_object.streams.filter(only_audio=True).first()
+        audio_stream.download()
+        return jsonify({'message': 'Audio download completed successfully'})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+@app.route('/download_video', methods=['POST'])
+def download_video():
+    try:
+        data = request.get_json()
+        link = data.get('link')
+        youtube_object = YouTube(link)
+        video_stream = youtube_object.streams.get_highest_resolution()
+        video_stream.download()
+        return jsonify({'message': 'Video download completed successfully'})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
